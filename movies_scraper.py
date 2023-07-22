9dfb3435454896c2f828be283338070be54f8ca4
MirjaMahdi885 <136712812+MirjaMahdi885@users.noreply.github.com>
2023-07-22 11:35:12 +0600

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 url_list = {}
-api_key = "c7973dc47796f4547d5e0a47a0796812f24c7c5e"
+api_key = "4b745c2b496ad8634f653f4068983b6bc8ac01ea"
 
 
 def search_movies(query):
@@ -32,7 +32,7 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://v2links.com/api?api={api_key}&url={i['href']}"
+            url = f"https://clicksfly.com/api?api={api_key}&url={i['href']}"
             response = requests.get(url)
             link = response.json()
             final_links[f"{i.text}"] = link['shortenedUrl']
