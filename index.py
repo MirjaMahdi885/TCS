cf0a821d6f768a6582821508d7116a556ae14752
MirjaMahdi885 <136712812+MirjaMahdi885@users.noreply.github.com>
2023-10-05 11:47:52 +0600

    Update index.py

--- index.py
+++ index.py
@@ -9,12 +9,12 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://tcs885-git-main-mirjamahdi885.vercel.app"
+URL = os.getenv("URL")
 bot = Bot(TOKEN)
 
 
 def welcome(update, context) -> None:
-    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to TCS Movies.\n"
+    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SB Movies.\n"
                               f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
     update.message.reply_text("👇 Enter Movie Name 👇")
 
@@ -31,7 +31,7 @@ def find_movie(update, context):
         reply_markup = InlineKeyboardMarkup(keyboards)
         search_results.edit_text('Search Results...', reply_markup=reply_markup)
     else:
-        search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.\nPlease correct your movie name here- https://www.google.com/ ')
+        search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.')
 
 
 def movie_result(update, context) -> None:
@@ -39,47 +39,3 @@ def movie_result(update, context) -> None:
     s = get_movie(query.data)
     response = requests.get(s["img"])
     img = BytesIO(response.content)
-    query.message.reply_photo(photo=img, caption=f"🎥 {s['title']}")
-    link = ""
-    links = s["links"]
-    for i in links:
-        link += "🎬" + i + "\n" + links[i] + "\n\n"
-    caption = f"Try not to use GDTOT links\n⚡ Fast Download Links :-\n\n{link}"
-    if len(caption) > 4095:
-        for x in range(0, len(caption), 4095):
-            query.message.reply_text(text=caption[x:x+4095])
-    else:
-        query.message.reply_text(text=caption)
-
-
-def setup():
-    update_queue = Queue()
-    dispatcher = Dispatcher(bot, update_queue, use_context=True)
-    dispatcher.add_handler(CommandHandler('start', welcome))
-    dispatcher.add_handler(MessageHandler(Filters.text, find_movie))
-    dispatcher.add_handler(CallbackQueryHandler(movie_result))
-    return dispatcher
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return 'Hello World!'
-
-
-@app.route('/{}'.format(TOKEN), methods=['GET', 'POST'])
-def respond():
-    update = Update.de_json(request.get_json(force=True), bot)
-    setup().process_update(update)
-    return 'ok'
-
-
-@app.route('/setwebhook', methods=['GET', 'POST'])
-def set_webhook():
-    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
